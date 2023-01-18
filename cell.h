7954546d48a00516e027f6eb2e2cc83fcf5b1272
kshchuk <kshchuk@gmail.com>
2023-01-18 14:36:04 +0200

    Added some useful methods

--- cell.h
+++ cell.h
@@ -1,11 +1,18 @@
 #ifndef CELL_H
 #define CELL_H
 
+struct Point {int x = 0, y = 0; };
 
 class Cell
 {
+private:
+    Point position;
+
 public:
     Cell();
+    Cell(Point startingPosition);
+
+    Point getPosition() const;
 };
 
 #endif // CELL_H
--- frame.cpp
+++ frame.cpp
@@ -4,3 +4,28 @@ Frame::Frame()
 {
 
 }
+
+Frame::Frame(Cell* cell)
+{
+    this->cellPtr = cell;
+}
+
+bool Frame::isCell()
+{
+    return cellPtr != nullptr;
+}
+
+void Frame::setCell(Cell* cell)
+{
+    this->cellPtr = cell;
+}
+
+Cell* Frame::getCell()
+{
+    return cellPtr;
+}
+
+void Frame::makeEmpty()
+{
+    cellPtr = nullptr;
+}
