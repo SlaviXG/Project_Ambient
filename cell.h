00deadf7e87642000bd30b4322bbc494221d638f
kshchuk <kshchuk@gmail.com>
2023-03-28 15:31:37 +0300

    Updated TODO

--- cell.h
+++ cell.h
@@ -85,14 +85,13 @@ namespace environment
          */
         void die() { environment->RemoveCell(this); };
 
+
         /**
-         * @brief Make choice and do action
+         * @brief Make choice and perform action
          * 
-         * @return std::pair<bool, double> 
-         * bool - is aggressiveness changed,
-         * double - new aggressiveness
+         * @return Action performed 
          */
-        std::pair<bool, double> act() {/* TODO */}
+        actions act() {/* TODO */}
         int act(std::vector<double> inputs);
         void duplicate();
 
