f6b4c446c8af316707c5177df888d141c57b8f71
kshchuk <kshchuk@gmail.com>
2023-05-18 01:25:43 +0300

    Implemented GenerateRandomCells

--- GameController.h
+++ GameController.h
@@ -44,6 +44,7 @@ namespace controller
     {
     public:
         virtual void addCell(const Point &point) = 0;
+        virtual void addCell(const Point &point,  const std::vector<int>& countOfWeights) = 0;
         virtual void start() = 0;
         virtual void stop() = 0;
         virtual void pause() = 0;
@@ -68,7 +69,7 @@ namespace controller
             this->addCell(cellptr);
         }
 
-        inline void addCell(const Point &point,  std::vector<int> countOfWeights)
+        inline void addCell(const Point &point, const std::vector<int>& countOfWeights) override
         {
             auto cellptr = environment->AddCell(point, countOfWeights);
             this->addCell(cellptr);
@@ -122,6 +123,59 @@ namespace controller
             for (auto& logger : loggers)
             {
                 logger->log(message);
+            }
+        }
+
+        void GenerateRandomCells(size_t cell_count) {
+            assert(environment != nullptr);
+
+            for (size_t i = 0; i < cell_count; ++i) {
+                auto pos = environment::RandomGenerator::generateRandomPoint({0, 0}, {environment->getHeight(), environment->getWidth()});
+                this->addCell(pos);
+            }
+        }
+
+        void GenerateRandomCells(size_t cell_count, const std::vector<int>& countOfWeights) {
+            assert(environment != nullptr);
+
+            for (size_t i = 0; i < cell_count; ++i) {
+                auto pos = environment::RandomGenerator::generateRandomPoint({0, 0}, {environment->getHeight(), environment->getWidth()});
+                this->addCell(pos, countOfWeights);
+            }
+        }
+
+        /**
+         * @brief GenerateRandomCells
+         * @param cell_count
+         * @param top_left Top left corner of the bounding box
+         * @param bottom_right Bottom right corner of the bounding box
+         */
+        void GenerateRandomCells(size_t cell_count, const Point& top_left, const Point& bottom_right) {
+            assert(environment != nullptr);
+            assert(environment->checkPositionCorrectness(top_left));
+            assert(environment->checkPositionCorrectness(bottom_right));
+
+            for (size_t i = 0; i < cell_count; ++i) {
+                auto pos = environment::RandomGenerator::generateRandomPoint(top_left, bottom_right);
+                this->addCell(pos);
+            }
+        }
+
+        /**
+         * @brief GenerateRandomCells
+         * @param cell_count
+         * @param countOfWeights
+         * @param top_left Top left corner of the bounding box
+         * @param bottom_right Bottom right corner of the bounding box
+         */
+        void GenerateRandomCells(size_t cell_count, const std::vector<int>& countOfWeights, const Point& top_left, const Point& bottom_right) {
+            assert(environment != nullptr);
+            assert(environment->checkPositionCorrectness(top_left));
+            assert(environment->checkPositionCorrectness(bottom_right));
+
+            for (size_t i = 0; i < cell_count; ++i) {
+                auto pos = environment::RandomGenerator::generateRandomPoint(top_left, bottom_right);
+                this->addCell(pos, countOfWeights);
             }
         }
 
