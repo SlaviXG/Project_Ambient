c4df53335bf1e57ec86fbb030ee4d436c327ef2e
kshchuk <kshchuk@gmail.com>
2023-06-03 12:34:09 +0300

    Added CMakeLists.txt
    How to build: https://doc.qt.io/qt-6/cmake-build-on-cmdline.html

---
+++ build/CMakeFiles/YourProjectName.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Projects/Project_Ambient/CellView.cpp" "CMakeFiles/YourProjectName.dir/CellView.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/CellView.cpp.obj.d"
+  "D:/Projects/Project_Ambient/Environment.cpp" "CMakeFiles/YourProjectName.dir/Environment.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/Environment.cpp.obj.d"
+  "D:/Projects/Project_Ambient/EnvironmentScene.cpp" "CMakeFiles/YourProjectName.dir/EnvironmentScene.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/EnvironmentScene.cpp.obj.d"
+  "D:/Projects/Project_Ambient/GameController.cpp" "CMakeFiles/YourProjectName.dir/GameController.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/GameController.cpp.obj.d"
+  "D:/Projects/Project_Ambient/Genotype.cpp" "CMakeFiles/YourProjectName.dir/Genotype.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/Genotype.cpp.obj.d"
+  "D:/Projects/Project_Ambient/build/YourProjectName_autogen/mocs_compilation.cpp" "CMakeFiles/YourProjectName.dir/YourProjectName_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/YourProjectName_autogen/mocs_compilation.cpp.obj.d"
+  "D:/Projects/Project_Ambient/cell.cpp" "CMakeFiles/YourProjectName.dir/cell.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/cell.cpp.obj.d"
+  "D:/Projects/Project_Ambient/celltracker.cpp" "CMakeFiles/YourProjectName.dir/celltracker.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/celltracker.cpp.obj.d"
+  "D:/Projects/Project_Ambient/frame.cpp" "CMakeFiles/YourProjectName.dir/frame.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/frame.cpp.obj.d"
+  "D:/Projects/Project_Ambient/genepool.cpp" "CMakeFiles/YourProjectName.dir/genepool.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/genepool.cpp.obj.d"
+  "D:/Projects/Project_Ambient/main.cpp" "CMakeFiles/YourProjectName.dir/main.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/main.cpp.obj.d"
+  "D:/Projects/Project_Ambient/mainwindow.cpp" "CMakeFiles/YourProjectName.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/YourProjectName.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
